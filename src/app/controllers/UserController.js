997b5958083194e0d8d8082f93478f75e014f14e
Alexandre AGIS. Garcia Ishikawa dos Santos <alexandre.ishikawa@DESKTOP-UA3QJ59.neotix.net>
2020-02-27 10:21:46 -0300

    User Creation

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,45 @@
+import * as Yup from "yup";
+import bcrypt from "bcryptjs";
+
+import User from "../models/User";
+
+class UserController {
+  async store(req, res, next) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      lastName: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: "Validation failed." });
+    }
+
+    const { name, lastName, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ error: "User already exists." });
+    }
+
+    const user = new User({
+      name,
+      lastName,
+      email,
+      password: await bcrypt.hash(password, 10)
+    });
+
+    user
+      .save()
+      .then(savedUser => res.json(savedUser))
+      .catch(err => next(err));
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
+import UserController from "./app/controllers/UserController";
+
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Node boilerplate" });
-});
+routes.post("/register", UserController.store);
 
 export default routes;
